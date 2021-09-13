7990a56f458c0b46ebad9d87bb385a3d6e0f31dc
Aimee Rawlings <aimeeclairerawlings@gmail.com>
2021-09-14 00:06:38 +0200

    Added images to search

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,35 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 import './Dictionary.css'
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
     let [result, setResult] = useState(null);
+    let [photos, setPhoto] = useState(null);
 
     function handleResponse(response) {
         setResult(response.data[0]);
+    }
+
+     function handlePexelsResponse(response) {
+        setPhoto(response.data.photos);
     }
 
     function searchKeyword(event) {
         event.preventDefault();
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`
-        axios.get(apiUrl).then(handleResponse)
+        axios.get(apiUrl).then(handleResponse);
+
+        
+        let pexelsApiKey = "563492ad6f91700001000001806f9bed4be043d2b4e98eb06e907ada";
+        
+        let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+        let headers = {Authorization: `Bearer ${pexelsApiKey}`};
+
+        axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
 
     function changeKeyword(event) {
@@ -32,6 +46,8 @@ export default function Dictionary() {
         <div className="Result">
             <Result results={result}/>
         </div>
+
+        <Photos photos={photos} />
 
         </div>
     )
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,13 +6,16 @@ export default function Synonym(props) {
         return (
             <ul className="Synonyms">
         {props.synonyms.map(function (synonym, index) {
+            if (index > 8) {
             return (
                 <li style={{textAlign: 'left'}}>
                 <div key={index}>
                     {synonym}
                 </div>
                  </li>
-            );
+            )} else {
+            return null;
+            };
             })}
         </ul>
         )
